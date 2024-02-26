811251ce54d3afaf4fad2088fe2774719ee57244
keremcant <keremcanturgt@gmail.com>
2024-02-26 10:52:11 +0300

    fixes birfatura

--- odoo/custom_addons/birFatura_efatura/models/res_config_settings.py
+++ odoo/custom_addons/birFatura_efatura/models/res_config_settings.py
@@ -1,20 +1,64 @@
-from odoo import models, fields
+import zeep
+
+from odoo import models, fields, api
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _name = 'fatura.config.settings'
 
-    isyeri = fields.Char(string='Isyeri')
-    username = fields.Char(string='Username')
-    password = fields.Char(string='Password')
-    catalog = fields.Char(string='Catalog')
-    status = fields.Selection([('draft', 'Draft')])
+    isyeri = fields.Char(string='Isyeri', default="SUBECOMITFY")
+    username = fields.Char(string='Username', default="USERCOMITFY")
+    password = fields.Char(string='Password', default="USERCOMITFY")
+    catalog = fields.Char(string='Catalog', default="EFATURATEST")
+    trnstartdate = fields.Date()
+    trnenddate = fields.Date()
 
     birfatura_canli_test = fields.Selection([
-        ('0', 'Canli'),
-        ('1', 'Test'),
-    ], 'Live or test integration', config_parameter='birfatura_canli_test', default='0')
+        ('Canli', 'Canli'),
+        ('Test', 'Test'),
+    ], 'Live or test integration', config_parameter='birfatura_canli_test', default='Canli')
+
+    status = fields.Selection([
+        ('Aktif', 'Aktif'),
+        ('Pasif', 'Pasif'),
+    ], 'Live or test integration', config_parameter='status', default='Pasif')
+
+    gbpk = fields.Selection([
+        ('GB', 'Gönderilen Fatura'),
+        ('PK', 'Gelen Fatura'),
+    ], 'Live or test integration', config_parameter='gbpk', default='GB')
+
+    dateisinv = fields.Selection([
+        ('true', 'Evet'),
+        ('false', 'Hayır'),
+    ], 'Live or test integration', config_parameter='isinv', default='true')
+
+    @api.model_create_multi
+    def create(self, vals):
+        wsdl = 'https://outapi-test.einvoiceplatform.com/efatura.svc?wsdl'
+        client = zeep.Client(wsdl=wsdl)
+        res = super(ResConfigSettings, self).create(vals)
+        print(f"Oluşturulan isyeri adı: {res.isyeri}")
+        print(f"Oluşturulan username adı: {res.username}")
+        print(f"Oluşturulan password adı: {res.password}")
+        print(f"Oluşturulan catalog adı: {res.catalog}")
+        print(f"Oluşturulan birfatura_canli_test: {res.birfatura_canli_test}")
+        trnenddate = res.trnenddate.strftime('%d.%m.%Y')
+        trnstartdate = res.trnstartdate.strftime('%d.%m.%Y')
+        print(f"Oluşturulan trnenddate: {trnenddate}")
+        print(f"Oluşturulan trnstartdate: {trnstartdate}")
+
+        clientUser = {
+            'Catolog': res.catalog,
+            'İsyeri': res.isyeri,
+            'Password': res.password,
+            'Username': res.username
+        }
+
+        sonuc = client.service.GetInvoiceList(clientUser, res.gbpk, trnstartdate, trnenddate, res.dateisinv)
 
 
 
+        print(f"sonuc: {sonuc.text}")
 
+        return res
